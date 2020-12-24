de561db4d47bc53cc9367772a0deac0446a9831a
James Ransome <jamesransome@msn.com>
2020-12-24 15:20:41 +0000

    Lesson 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const express = require('express');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(PORT, () => console.log('Server running on port', PORT));
