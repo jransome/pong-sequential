665478954fe5a48d063bdd70559b912e3c419d65
James Ransome <jamesransome@msn.com>
2020-12-24 15:52:25 +0000

    Lesson 3

--- public/engine.js
+++ public/engine.js
@@ -2,6 +2,7 @@ const { Engine, Events, Bodies, World, Resolver, Runner } = window.Matter;
 
 Resolver._restingThresh = 0.01; // Reduce velocity threshold required for engine to calculate ball bounces
 
+const COURT_SIZE = { x: 1000, y: 600 };
 const BALL_RADIUS = 15;
 
 const engine = Engine.create();
@@ -9,12 +10,20 @@ const engine = Engine.create();
 // Create the ball body
 const ball = Bodies.circle(100, 100, BALL_RADIUS, { label: 'ball' });
 
+const wallProperties = { isStatic: true };
+const walls = [
+  Bodies.rectangle(COURT_SIZE.x / 2, -25, COURT_SIZE.x, 50, wallProperties), // top
+  Bodies.rectangle(COURT_SIZE.x / 2, COURT_SIZE.y + 25, COURT_SIZE.x, 50, wallProperties), // bottom
+  Bodies.rectangle(-25, COURT_SIZE.y / 2, 50, COURT_SIZE.y, wallProperties), // left
+  Bodies.rectangle(COURT_SIZE.x + 25, COURT_SIZE.y / 2, 50, COURT_SIZE.y, wallProperties), // right
+];
+
 Events.on(engine, 'afterUpdate', () => {
   window.ballPosition = ball.position;
 });
 
 // Add physics bodies to the matterjs world
-World.add(engine.world, [ball]);
+World.add(engine.world, [ball, ...walls]);
 
 const runner = Runner.create();
 Runner.run(runner, engine);
--- public/render.js
+++ public/render.js
@@ -10,14 +10,25 @@ const renderer = new P5((sketch) => {
     sketch.circle(position.x, position.y, diameter);
   };
 
+  const renderCourt = () => {
+    sketch.stroke(255);
+    sketch.drawingContext.setLineDash([10, 10]);
+    sketch.line(COURT_SIZE.x / 2, 0, COURT_SIZE.x / 2, COURT_SIZE.y);
+
+    sketch.drawingContext.setLineDash([]);
+    sketch.fill(0, 0);
+    sketch.rect(0, 0, COURT_SIZE.x, COURT_SIZE.y);
+  };
+
   // This function is called once to set up the canvas
   sketch.setup = () => {
     sketch.createCanvas(COURT_SIZE.x, COURT_SIZE.y);
   };
-  
+
   // This function is called on every frameand is used to update the canvas
   sketch.draw = () => {
     sketch.background(0);
+    renderCourt();
     renderBall(window.ballPosition, BALL_RADIUS * 2);
   };
 }, document.getElementById('game'));
